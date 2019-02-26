d333bb5c13d8c6ac1c474ef58aed73685f65ae87
Mark Riddoch <mark@dianomic.com>
2019-02-26 13:40:31 +0000

    FOGL-2531 Initial version

---
+++ include/ifttt.h
@@ -0,0 +1,23 @@
+#ifndef _IFTTT_H
+#define _IFTTT_H
+#include <config_category.h>
+#include <string>
+#include <logger.h>
+
+/**
+ * A simple IFTTT notification class that sends a message
+ * to the IFTTT button_pressed aptlet to simulate the pressign
+ * of a button. IFTTT will then perform whatever action is
+ * associated with that button press.
+ */
+class IFTTT {
+	public:
+		IFTTT(ConfigCategory *config);
+		~IFTTT();
+		void	notify(const std::string& notificationName, const std::string& triggerReason, const std::string& message);
+		void	reconfigure(const std::string& newConfig);
+	private:
+		std::string	m_key;
+		std::string	m_trigger;
+};
+#endif
