cd2fd9cd6d24e42f005057a1afd8bcf14e4c9c72
pintomax <massimiliano.pinto@gmail.com>
2019-08-09 12:52:44 +0200

    Update plugin.cpp

--- plugin.cpp
+++ plugin.cpp
@@ -96,6 +96,7 @@ bool plugin_deliver(PLUGIN_HANDLE handle,
 							deliveryName.c_str(), notificationName.c_str(), triggerReason.c_str(), message.c_str());
 	IFTTT *ifttt = (IFTTT *)handle;
 	ifttt->notify(notificationName, triggerReason, message);
+	return true;
 }
 
 /**
